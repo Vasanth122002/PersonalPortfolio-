c099baeb46e2174d32984c23f5db46dd8b014179
Vasanth122002 <vasanthm200212@gmail.com>
2024-11-11 18:42:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("toggle-mode").addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
+
+document.querySelectorAll("header nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
