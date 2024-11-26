7e5aeb89726406a45930320ca1396f52ec0174b0
Vasanth122002 <vasanthm200212@gmail.com>
2024-11-26 21:24:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Dark mode toggle
+
 document.getElementById("toggle-mode").addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
 });
 
-// Smooth scroll for navigation links
 document.querySelectorAll("header nav a").forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
